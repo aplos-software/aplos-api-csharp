ebd2a9b883edb0b34b120feeca78c0e8e49fbb2e
adam-aplos <141665470+adam-aplos@users.noreply.github.com>
2023-08-07 14:15:53 -0500

    update base url to new subdomain

--- src/AplosApi/Constants.cs
+++ src/AplosApi/Constants.cs
@@ -5,7 +5,7 @@ namespace AplosApi
 {
     internal static class Constants
     {
-        public const string ApiUrlBase = "https://www.aplos.com/hermes/api/v1/";
+        public const string ApiUrlBase = "https://app.aplos.com/hermes/api/v1/";
         public const string RestClientId = "X-Rest-Client-Id";
         public const string AplosApiClientTypeHeaderKey = "X-Aplos-Api-Client-Type";
         public const string AplosApiClientTypeHeaderValue = "aplos-api-client-csharp";
